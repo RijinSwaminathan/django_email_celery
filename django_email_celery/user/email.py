9fc9f71bdcfda4d70c20f69122dd366910a328ec
rijindiagun <rijin.np@diagunmail.com>
2021-07-15 17:57:27 +0530

    added html template.

--- django_email_celery/user/email.py
+++ django_email_celery/user/email.py
@@ -1,16 +1,17 @@
-from django.template import Context
 from django.template.loader import render_to_string
-from django.core.mail import EmailMessage
+from django.core.mail import EmailMessage, EmailMultiAlternatives
 from django.conf import settings
 
 
-def send_otp_mail(otp, mail_id):
+def send_otp_mail(time_otp, mail_id):
     context = {
-        'otp': otp,
+        'time_otp': time_otp,
         'mail_id': mail_id,
     }
-    email_subject = 'Account verification'
-    email_body = render_to_string('otp_email_msg.txt', context)
-    email = EmailMessage(email_subject, email_body, settings.EMAIL_HOST_USER, [mail_id, ],
-                         )
+    email_subject = 'Account verification',
+    email_text_body = render_to_string('otp_email_msg.txt', context)
+    email_html_body = render_to_string('user_verification.html', context)
+    email = EmailMultiAlternatives(email_subject, email_text_body, settings.EMAIL_HOST_USER,
+                                   [mail_id, ], )
+    email.attach_alternative(email_html_body, "text/html")
     return email.send(fail_silently=False)
